c3669d0aade712a41d0151c73047f56fe43961b7
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2024-06-28 22:20:31 +0530

    refresh_token

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.post('/register', usersController.registerUser);
 router.post('/login', usersController.login);
+router.post('/token', usersController.getTokenByRfToken);
 router.get('/me', isAuth, usersController.me);
+router.delete('/logout', isAuth, usersController.logout);
 router.delete('/deactivate', isAuth, usersController.deleteUser);
 
 module.exports = router;
